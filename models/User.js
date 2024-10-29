77fcffadf503b018c6d3a0d5bc7f4d0ec6c9b5b6
tian <imalasong2024@gmail.com>
2024-10-29 15:38:05 -0400

    Update User model with gender field and validation messages

--- models/User.js
+++ models/User.js
@@ -1,37 +1,32 @@
+// models/User.js
+
 import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: [true, '请输入姓名'],
   },
   email: {
     type: String,
-    required: true,
-    unique: true
+    required: [true, '请输入邮箱'],
+    unique: true,
   },
   password: {
     type: String,
-    required: true
+    required: [true, '请输入密码'],
   },
-  birthDate: {    // 新增生日字段
+  birthDate: {
     type: Date,
-    required: true
+    required: [true, '请输入出生日期'],
+  },
+  gender: {
+    type: String,
+    enum: ['M', 'F'],
+    required: [true, '请选择性别'],
   }
 }, {
   timestamps: true
-});
-
-// 添加虚拟字段计算年龄
-UserSchema.virtual('age').get(function() {
-  const today = new Date();
-  const birthDate = this.birthDate;
-  let age = today.getFullYear() - birthDate.getFullYear();
-  const m = today.getMonth() - birthDate.getMonth();
-  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-    age--;
-  }
-  return age;
 });
 
 export default mongoose.models.User || mongoose.model('User', UserSchema);
